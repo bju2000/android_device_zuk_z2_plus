4e3ab861b232664c32ddbdf6a018496948852aba
LuK1337 <priv.luk@gmail.com>
2017-07-12 00:39:08 +0200

    camera: Add libc_shim
    Change-Id: Ie0735585c6187b756c5fb48c76ea8835bce96630

--- device.mk
+++ device.mk
@@ -211,6 +211,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
 
+#Lib Shims
+PRODUCT_PACKAGES += \
+    libc_shim
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
